f4d0db7d72b26eef15b834c67c263e5e315e7f00
YeChan39 <43803451+YeChan39@users.noreply.github.com>
2018-10-13 00:47:07 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -10,6 +10,7 @@ import urllib
 import bs4
 import urllib.request
 import bs4
+import 급식
 
 
 client = discord.Client()
@@ -109,6 +110,7 @@ async def on_message(message):
         embed.add_field(name='!네코', value='!네코 라고 적으면 2D 고양이 이미지가 나옵니다', inline=False)
         embed.add_field(name='!실시간검색어, !실검', value='!실시간검색어, !실검 이라고 적으면 네이버의 실시간 검색어 순위가 나타납니다.', inline=False)
         embed.add_field(name='!번역 번역할문자', value='!번역 번역할문자 이라고 적으면 번역할 문자를 번역한 링크가 나타납니다. ("띄어쓰기를 하시면 안됩니다. _,-등으로 구분해주세요.")', inline=False)
+        embed.add_field(name='!영화순위', value='영화를 1~20순위로 나눈 영화순위 정보를 제공합니다.', inline=False)
 
         await client.send_message(channel,embed=embed)
 
@@ -196,12 +198,41 @@ async def on_message(message):
         todayFeelingTemp = todayFeelingTemp1.text.strip()  # 체감온도
         print(todayFeelingTemp)
 
+        todayMiseaMongi1 = bsObj.find('div', {'class': 'sub_info'})
+        todayMiseaMongi2 = todayMiseaMongi1.find('div', {'class': 'detail_box'})
+        todayMiseaMongi3 = todayMiseaMongi2.find('dd')
+        todayMiseaMongi = todayMiseaMongi3.text  # 미세먼지
+        print(todayMiseaMongi)
+
         tomorrowBase = bsObj.find('div', {'class': 'table_info weekly _weeklyWeather'})
         tomorrowTemp1 = tomorrowBase.find('li', {'class': 'date_info'})
         tomorrowTemp2 = tomorrowTemp1.find('dl')
         tomorrowTemp3 = tomorrowTemp2.find('dd')
-        tomorrowTemp = tomorrowTemp3.text.strip()  # 내일 오전,오후온도
+        tomorrowTemp = tomorrowTemp3.text.strip()  # 오늘 오전,오후온도
         print(tomorrowTemp)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowMoring1 = tomorrowAreaBase.find('div', {'class': 'main_info morning_box'})
+        tomorrowMoring2 = tomorrowMoring1.find('span', {'class': 'todaytemp'})
+        tomorrowMoring = tomorrowMoring2.text.strip()  # 내일 오전 온도
+        print(tomorrowMoring)
+
+        tomorrowValue1 = tomorrowMoring1.find('div', {'class': 'info_data'})
+        tomorrowValue = tomorrowValue1.text.strip()  # 내일 오전 날씨상태, 미세먼지 상태
+        print(tomorrowValue)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowAllFind = tomorrowAreaBase.find_all('div', {'class': 'main_info morning_box'})
+        tomorrowAfter1 = tomorrowAllFind[1]
+        tomorrowAfter2 = tomorrowAfter1.find('p', {'class': 'info_temperature'})
+        tomorrowAfter3 = tomorrowAfter2.find('span', {'class': 'todaytemp'})
+        tomorrowAfterTemp = tomorrowAfter3.text.strip()  # 내일 오후 온도
+        print(tomorrowAfterTemp)
+
+        tomorrowAfterValue1 = tomorrowAfter1.find('div', {'class': 'info_data'})
+        tomorrowAfterValue = tomorrowAfterValue1.text.strip()
+
+        print(tomorrowAfterValue)  # 내일 오후 날씨상태,미세먼지
 
         embed = discord.Embed(
             title=learn[1]+ ' 날씨 정보',
@@ -211,7 +242,15 @@ async def on_message(message):
         embed.add_field(name='현재온도', value=todayTemp+'˚', inline=False)  # 현재온도
         embed.add_field(name='체감온도', value=todayFeelingTemp, inline=False)  # 체감온도
         embed.add_field(name='현재상태', value=todayValue, inline=False)  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
-        embed.add_field(name='내일 오전/오후 날씨', value=todayFeelingTemp, inline=False)  # 내일날씨
+        embed.add_field(name='현재 미세먼지 상태', value=todayMiseaMongi, inline=False)  # 오늘 미세먼지
+        embed.add_field(name='오늘 오전/오후 날씨', value=tomorrowTemp, inline=False)  # 오늘날씨 # color=discord.Color.blue()
+        embed.add_field(name='**----------------------------------**',value='**----------------------------------**', inline=False)  # 구분선
+        embed.add_field(name='내일 오전온도', value=tomorrowMoring+'˚', inline=False)  # 내일오전날씨
+        embed.add_field(name='내일 오전날씨상태, 미세먼지 상태', value=tomorrowValue, inline=False)  # 내일오전 날씨상태
+        embed.add_field(name='내일 오후온도', value=tomorrowAfterTemp + '˚', inline=False)  # 내일오후날씨
+        embed.add_field(name='내일 오후날씨상태, 미세먼지 상태', value=tomorrowAfterValue, inline=False)  # 내일오후 날씨상태
+
+
 
         await client.send_message(message.channel,embed=embed)
 
@@ -549,16 +588,25 @@ async def on_message(message):
         bsObj = bs4.BeautifulSoup(html, "html.parser")
         realTimeSerach1 = bsObj.find('div', {'class': 'ah_roll_area PM_CL_realtimeKeyword_rolling'})
         realTimeSerach2 = realTimeSerach1.find('ul', {'class': 'ah_l'})
-        realTimeSerach3 = realTimeSerach2.text
-        realTimeSerach4 = realTimeSerach3.strip()
-        print(realTimeSerach4)
+        realTimeSerach3 = realTimeSerach2.find_all('li')
+
+
         embed = discord.Embed(
             title='네이버 실시간 검색어',
             description='실시간검색어',
             colour=discord.Colour.green()
         )
-        embed.add_field(name='현재 실시간 검색어 순위입니다.', value=realTimeSerach4, inline=False)
-        await client.send_message(message.channel,embed=embed)
+        for i in range(0,19):
+            realTimeSerach4 = realTimeSerach3[i]
+            realTimeSerach5 = realTimeSerach4.find('span', {'class': 'ah_k'})
+            realTimeSerach = realTimeSerach5.text.replace(' ', '')
+            realURL = 'https://search.naver.com/search.naver?ie=utf8&query='+realTimeSerach
+            print(realTimeSerach)
+            embed.add_field(name=str(i+1)+'위', value='**----'+realTimeSerach +'----**'+ '\n'+realURL, inline=False)
+
+        await client.send_message(message.channel, embed=embed)
+
+
 
     if message.content.startswith('!번역'):
         learn = message.content.split(" ")
@@ -587,16 +635,83 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
         #"http://www.gifbin.com/random"
 
+
+    if message.content.startswith('!영화순위'):
+        # http://ticket2.movie.daum.net/movie/movieranklist.aspx
+        i1 = 0 # 랭킹 string값
+        embed = discord.Embed(
+            title = "영화순위",
+            description = "영화순위입니다.",
+            colour= discord.Color.red()
+        )
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        url = 'http://ticket2.movie.daum.net/movie/movieranklist.aspx'
+        print(url)
+        req = Request(url, headers=hdr)
+        html = urllib.request.urlopen(req)
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        moviechartBase = bsObj.find('div', {'class': 'main_detail'})
+        moviechart1 = moviechartBase.find('ul', {'class': 'list_boxthumb'})
+        moviechart2 = moviechart1.find_all('li')
+
+        for i in range(0, 20):
+            i1 = i1+1
+            stri1 = str(i1) # i1은 영화랭킹을 나타내는데 사용됩니다
+            print()
+            print(i)
+            print()
+            moviechartLi1 = moviechart2[i]  # ------------------------- 1등랭킹 영화---------------------------
+            moviechartLi1Div = moviechartLi1.find('div', {'class': 'desc_boxthumb'})  # 영화박스 나타내는 Div
+            moviechartLi1MovieName1 = moviechartLi1Div.find('strong', {'class': 'tit_join'})
+            moviechartLi1MovieName = moviechartLi1MovieName1.text.strip()  # 영화 제목
+            print(moviechartLi1MovieName)
+
+            moviechartLi1Ratting1 = moviechartLi1Div.find('div', {'class': 'raking_grade'})
+            moviechartLi1Ratting2 = moviechartLi1Ratting1.find('em', {'class': 'emph_grade'})
+            moviechartLi1Ratting = moviechartLi1Ratting2.text.strip()  # 영화 평점
+            print(moviechartLi1Ratting)
+
+            moviechartLi1openDay1 = moviechartLi1Div.find('dl', {'class': 'list_state'})
+            moviechartLi1openDay2 = moviechartLi1openDay1.find_all('dd')  # 개봉날짜, 예매율 두개포함한 dd임
+            moviechartLi1openDay3 = moviechartLi1openDay2[0]
+            moviechartLi1Yerating1 = moviechartLi1openDay2[1]
+            moviechartLi1openDay = moviechartLi1openDay3.text.strip()  # 개봉날짜
+            print(moviechartLi1openDay)
+            moviechartLi1Yerating = moviechartLi1Yerating1.text.strip()  # 예매율 ,랭킹변동
+            print(moviechartLi1Yerating)  # ------------------------- 1등랭킹 영화---------------------------
+            print()
+            embed.add_field(name='---------------랭킹'+stri1+'위---------------', value='\n영화제목 : '+moviechartLi1MovieName+'\n영화평점 : '+moviechartLi1Ratting+'점'+'\n개봉날짜 : '+moviechartLi1openDay+'\n예매율,랭킹변동 : '+moviechartLi1Yerating, inline=False) # 영화랭킹
+
+
+        await client.send_message(message.channel, embed=embed)
+
+
+    if message.content.startswith("!급식"):
+        embed = discord.Embed(
+            title='군포 E 비즈니스 고등학교 급식',
+            description='급식입니다.',
+            colour=discord.Colour.green()
+        )
+        embed.add_field(name='오늘', value=급식.lunchtext(), inline=False)
+        embed.add_field(name='내일', value=급식.lunchtextD1(), inline=False)
+        embed.add_field(name='모래', value=급식.lunchtextD2(), inline=False)
+        await client.send_message(message.channel, embed=embed)
+
     if message.content.startswith("!테스트"):
         embed = discord.Embed(
             title='테스트',
             description='ㅇㅇ',
             colour=discord.Colour.green()
         )
+        embed.add_field(name='테스트', value='[Discordapp](<https://discordapp.com>)', inline=False)
         embed.add_field(name='테스트중..', value="https://youtu.be/WptXk39wiIQ", inline=False)
-        embed.add_field(name='테스트중..', value="https://youtu.be/2kZVEUGLgy4", inline=False)
-        embed.add_field(name='테스트중..', value="https://youtu.be/z5A0joDLq38", inline=False)
         await client.send_message(message.channel, embed=embed)
+
+
+
+
+
+
 
         
 
