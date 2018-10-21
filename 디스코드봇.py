08ccc54240ac80b0de8ea8bf2ae22689ca8e3145
YeChan39 <43803451+YeChan39@users.noreply.github.com>
2018-10-22 01:01:23 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -836,6 +836,23 @@ async def on_message(message):
         print("랜덤수 값 :" + str(randomNum))
         print(emoji[randomNum])
         await client.send_message(message.channel, embed=discord.Embed(description=emoji[randomNum])) # 랜덤 이모티콘을 메시지로 출력합니다.
+    
+    if message.content.startswith('!주사위'):
+
+        randomNum = random.randrange(1, 7) # 1~6까지 랜덤수
+        print(randomNum)
+        if randomNum == 1:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: '+ ':one:'))
+        if randomNum == 2:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':two:'))
+        if randomNum ==3:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':three:'))
+        if randomNum ==4:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':four:'))
+        if randomNum ==5:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':five:'))
+        if randomNum ==6:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':six: '))
 
 
 
